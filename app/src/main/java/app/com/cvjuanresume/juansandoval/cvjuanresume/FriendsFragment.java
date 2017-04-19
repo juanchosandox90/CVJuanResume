326f1c06a721f272a0dc4cf3864f1ea6a6c9908f
juanchosandox90 <juansandoval222@gmail.com>
2017-04-19 10:53:52 -0500

    Sketch_CVJuanSandoval_Diagram_Classes_Layouts_1.0.0-0

---
+++ app/src/main/java/app/com/cvjuanresume/juansandoval/cvjuanresume/FriendsFragment.java
@@ -0,0 +1,45 @@
+package app.com.cvjuanresume.juansandoval.cvjuanresume;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jsandoval on 18/04/17.
+ */
+
+public class FriendsFragment extends Fragment {
+
+    public FriendsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
---
+++ app/src/main/java/app/com/cvjuanresume/juansandoval/cvjuanresume/NavDrawerItem.java
@@ -0,0 +1,36 @@
+package app.com.cvjuanresume.juansandoval.cvjuanresume;
+
+/**
+ * Created by jsandoval on 18/04/17.
+ */
+
+public class NavDrawerItem {
+
+    private boolean showNotify;
+    private String titile;
+
+    public NavDrawerItem(){
+
+    }
+
+    public NavDrawerItem(boolean showNotify, String titile){
+        this.showNotify = showNotify;
+        this.titile = titile;
+    }
+
+    public boolean isShowNotify(){
+        return showNotify;
+    }
+
+    public void setShowNotify(boolean showNotify){
+        this.showNotify = showNotify;
+    }
+
+    public String getTitile(){
+        return titile;
+    }
+
+    public void setTitle(String titile){
+        this.titile=titile;
+    }
+}
